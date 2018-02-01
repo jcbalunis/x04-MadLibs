ce45a192765bb5e1a9546f1585a7c901db7d12cc
jcbalunis <jbalunis@mail.csuchico.edu>
2018-01-31 17:58:05 -0800

    Daniel's Issues

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,22 @@
 
 int main()
 {
+std::string name;
+int number;
+std::string noun; 
+char symbole;
+
+    std::cout<< "Give me a name" <<std::endl;
+    std::cin>> name; 
+    std::cout<< "Give me a number"<<std::endl;
+    std::cin>> number;
+    std::cout<< "Give me a Noun"<<std::endl;
+    std::cin>> noun; 
+    std::cout<< "Give me a punctuation"<<std::endl;
+    std::cin>> symbole;
+    
+    std::cout<< "On thier way to work " <<name<< " encountered some issues. They got pulled over " <<number<< " times. " <<name<< " almost ran into a " <<noun<<
+    ". After reaching their destination, they exclaimed \" I made it finally" <<symbole<<"\"." <<std::endl;
 
   return 0;
 }
